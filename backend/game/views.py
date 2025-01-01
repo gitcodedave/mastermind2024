448189d314b329c5c0ef71392e73af22fd136916
gitcodedave <davidortegacode@gmail.com>
2024-12-31 21:25:57 -0800

    Initialized django enviroment: Installed debug toolbar, set up MySQL database, installed and configured Django Rest Framework, created serialializers.py file, partitioned django as backend.

---
+++ backend/game/views.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+# Create your views here.
+
+
