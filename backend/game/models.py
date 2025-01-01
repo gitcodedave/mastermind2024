9c3ffa2e06be200ffd20d747fe2aee227833b772
gitcodedave <davidortegacode@gmail.com>
2024-12-31 22:53:40 -0800

    Created new app for holding base user core, added database models for the game, added serializers for models

--- backend/game/models.py
+++ backend/game/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class PlayerProfile(models.Model):
+    difficulty = models.IntegerField(
+        validators=[MinValueValidator(4), MaxValueValidator(6)], default=4)
+    player = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    current_game = models.ForeignKey(
+        'Game', on_delete=models.CASCADE, related_name='game_players', blank=True, null=True)
+
+
+class Game(models.Model):
+    active_player = models.ForeignKey(
+        PlayerProfile, on_delete=models.CASCADE, related_name='active_player_games')
+    secret_number = models.CharField(max_length=6)
+    game_round = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)], default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_time = models.DurationField(blank=True, null=True)
+
+
+class Round(models.Model):
+    game = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name='rounds')
+    guess = models.CharField(max_length=6)
+    correct_numbers = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(6)])
+    correct_positions = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(6)])
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraints(
+                fields=['game', 'guess'], name='unique_guess')
+        ]
+
+
+class Leaderboard(models.Model):
+    RESULT_WIN = 'W'
+    RESULT_LOSS = 'L'
+    RESULT_CHOICES = [
+        (RESULT_WIN, 'Win'),
+        (RESULT_LOSS, 'Loss')
+    ]
+    result = models.CharField(max_length=1, choices=RESULT_CHOICES)
+    total_time = models.DurationField()
+    difficulty = models.IntegerField(
+        validators=[MinValueValidator(4), MaxValueValidator(6)])
+    player = models.ForeignKey(PlayerProfile, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constrains = [
+            models.UniqueConstraint(
+                fields=['player', 'game'], name='unique_player_game')
+        ]
---
+++ backend/game/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import PlayerProfile, Game, Round, Leaderboard
+
+
+class PlayerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayerProfile
+        fields = ['player', 'difficulty', 'current_game']
+
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ['id', 'active_player', 'secret_number', 'game_round', 'created_at', 'total_time']
+
+
+class RoundSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Round
+        fields = ['id', 'game', 'guess', 'correct_numbers', 'correct_positions', 'timestamp']
+
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leaderboard
+        fields = ['id', 'result', 'total_time', 'difficulty', 'player', 'game']
