f703da62c61ec242eefa9941e77d6e4d1cf9a8d7
gitcodedave <davidortegacode@gmail.com>
2025-01-01 19:25:12 -0800

    Created APIView for generating the new game data, Created a new mixin to handle gathering user data for each new View that can use the Access Token stored in user's cookies

--- backend/game/views.py
+++ backend/game/views.py
@@ -2,11 +2,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 
-from backend.game.models import PlayerProfile
-from backend.game.permissions import IsSuperUser
-from backend.game.serializers import PlayerProfileSerializer
+from .mixins import PlayerDataMixin
+from .models import PlayerProfile
+from .permissions import IsSuperUser
+from .serializers import GameSerializer, PlayerProfileSerializer
+import requests
 # Create your views here.
 
 class PlayerProfileViewSet(ModelViewSet):
@@ -29,3 +32,42 @@ class PlayerProfileViewSet(ModelViewSet):
             serializer = PlayerProfileSerializer(player)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
+
+class NewGameView(PlayerDataMixin, APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            player_data = self.get_player_data(request)
+            difficulty = player_data.get('difficulty')
+
+            new_game_data = {
+                "player": player_data['player'],
+                "secret_number": self.generate_random_number(difficulty=difficulty),
+                "game_round": 0
+            }
+
+            serializer = GameSerializer(data=new_game_data)
+            serializer.is_valid(raise_exception=True)
+            game = serializer.save()
+
+            active_player = PlayerProfile.objects.get(
+                player=player_data['player']
+            )
+            active_player.current_game = game
+            active_player.save()
+
+            return Response({}, status=status.HTTP_201_CREATED)
+        except requests.exceptions.RequestException as error:
+            return Response({'error': f'Error with external API: {str(error)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def generate_random_number(self, difficulty):
+        url = f'https://www.random.org/integers/?num={
+            difficulty}&min=0&max=7&col=1&base=10&format=plain&rnd=new'
+        response = requests.get(url)
+        if response.status_code != 200:
+            response.raise_for_status()
+
+        secret_number = response.text
+        return secret_number
