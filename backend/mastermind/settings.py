228f048d555123a183949ac0730a1e1494a76fc8
gitcodedave <davidortegacode@gmail.com>
2025-01-02 20:07:33 -0800

    Configured pytest with separate settings.py environment

--- backend/mastermind/settings.py
+++ backend/mastermind/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+TESTING = False
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
